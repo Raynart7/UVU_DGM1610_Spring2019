7ae46c2a97a313fa424d735b2a60ec2a12ae5e31
Emma Gillespie <emma7gillespie@gmail.com>
2019-01-30 17:05:32 -0700

    Created script "Variables" & "Functions" under "assignments."

---
+++ 2D_Game/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varitables : MonoBehaviour{
+   //Variables
+    public int number;
+    public float speed; 
+    public string name;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        /*
+        This
+        is
+        a
+        multiline
+        comment
+         */
+        // number = 10;
+        speed = 0.94f;
+        name = "Bob";
+
+        print(name +" is "+number+" years old!"+ speed);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(number,0,0);
+        
+    }
+}
