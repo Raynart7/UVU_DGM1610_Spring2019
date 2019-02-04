e3ca9123a890bed5b472882b71f2b633c8f76485
Emma Gillespie <emma7gillespie@gmail.com>
2019-02-04 12:55:28 -0700

    Created and scripted "Functions_2" subject Colors

---
+++ 2D_Game/Assets/Scripts/Assignments/Functions_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions_2 : MonoBehaviour {
+   public string colors;
+
+    void Start(){
+    
+       Rainbow(colors);
+}
+
+    
+    void Rainbow(string colorState){
+
+        if(colorState == "Green"){
+            print("Adding yellow to blue creates green.");
+        }
+        else if(colorState == "Purple"){
+            print("Adding blue and red together creates pruple.");
+        }
+        else if(colorState == "Orange"){
+            print("Adding red to yellow you'll get orange.");
+        }
+    }
+}
