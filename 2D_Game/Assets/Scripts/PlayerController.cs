91b77d191e59920e21d69211a52daeff7cc28755
Emma Gillespie <emma7gillespie@gmail.com>
2019-04-16 12:03:56 -0600

    Animations created: idle, walk, jump Art completed for: Dresser, Table, Stool.

--- 2D_Game/Assets/Scripts/PlayerController.cs
+++ 2D_Game/Assets/Scripts/PlayerController.cs
@@ -13,44 +13,70 @@ public class PlayerController : MonoBehaviour{
     public float groundCheckRadius; 
     public LayerMask whatIsGround;
 
+    public Animator animator;
+
 
 
 
 
     // Start is called before the first frame update
     void Start() {
-         }
+         animator = GetComponent<Animator>();
+         animator.SetBool("IsWalking" ,false);
+         animator.SetBool("IsJumping" ,false);
 
-         void FixedUpdate(){
+    }
+
+     void FixedUpdate(){
              grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
          }
  
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+        print(GetComponent<Rigidbody2D>().velocity.x);
+
         // Moves player Left and Right
         if(Input.GetKey(KeyCode.D)){
+            print("pressed D");
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            //moveVelocity = moveSpeed;
+            animator.SetBool("IsWalking" ,true);
+            transform.localScale = new Vector3(0.5f,0.5f,1f);
 
         }
-        if(Input.GetKey(KeyCode.A)){
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        else if(Input.GetKeyUp (KeyCode.D)){
+            animator.SetBool("IsWalking" ,false);
         }
+
+        if(Input.GetKey(KeyCode.A)){
+            print("pressed A");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            //moveVelocity = -moveSpeed;
+            animator.SetBool("IsWalking" ,true);
+            transform.localScale = new Vector3(-0.5f,0.5f,1f);
+        }
+
+        else if(Input.GetKeyUp (KeyCode.A)){
+            animator.SetBool("IsWalking" ,false);
 
         // Player jump
         if(Input.GetKeyDown(KeyCode.W) && grounded){
         Jump();
-    }
-    
-        if(GetComponent<Rigidbody2D>().velocity.x > 0)
-            transform.localScale = new Vector3(0.5f,0.5f,1f);
 
-        else if(GetComponent<Rigidbody2D>().velocity.x < 0)
-            transform.localScale = new Vector3(-0.5f,0.5f,1f);
+        }
+
+        if(grounded){
+            animator.SetBool("IsJumping" ,false);
+        }
+    }
 
 
         void Jump(){
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+            animator.SetBool("IsJumping" ,true);
         }
 
     }
