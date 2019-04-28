41cbd5543e64a42555e68e3de23c6667e181d05e
Emma Gillespie <emma7gillespie@gmail.com>
2019-04-27 22:38:52 -0600

    Game menu done. can start game through menu

---
+++ 2D_Game/Assets/Scripts/Load_Scene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Scene : MonoBehaviour
+{
+    
+    
+
+    public void Load(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);    
+    }
+
+
+
+}
